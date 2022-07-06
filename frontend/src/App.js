ceb716e4b7347a679f9e0cab8d8a79fc677c4427
jinhobyeong <jhb_0921@naver.com>
2022-07-07 03:08:12 +0900

    test : init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
