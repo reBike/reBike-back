43212682273a951da3e42b6961a69b2151fba9a1
Chaehy <chlee7670@cau.ac.kr>
2022-07-10 02:03:49 +0900

    style: add mypage shape ui

--- frontend/src/Page/MyPage.js
+++ frontend/src/Page/MyPage.js
@@ -1,5 +1,59 @@
+import * as React from "react";
+import MyPageNavigation from "../component/Mypage/MyPageNavigation";
+import MyTrashcan from "../component/Mypage/MyTrashcan";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+    Button,
+    CssBaseline,
+    Box,
+    Typography,
+    Container,
+    Link,
+    styled,
+    Grid,
+} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#E7F5EF",
+        },
+    },
+});
+
+
+const NaverLoginBtn = styled(Button)(({}) => ({
+    backgroundColor: "white",
+    "&:hover": {
+        color: "#6AED64",
+        backgroundColor: "#54B94E",
+        borderColor: "#54B94E",
+    },
+}));
+
 function MyPage(){
-    return <h1>MyPage</h1>;
+    return (
+        <Container
+            style={{
+                border: "solid",
+                borderColor: "#E7F5EF",
+                minWidth: "100%",
+                height: "100vh",
+        }}>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    mb: 2, mt: 20
+            }}>
+                <MyPageNavigation />
+                <Container>
+                    <MyTrashcan />
+                </Container>
+            </Box>
+        </Container>
+
+    );
 }
 
 export default MyPage;
