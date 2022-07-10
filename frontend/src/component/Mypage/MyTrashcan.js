2e671326c00e7b68333e0d97ac1632c55a907601
Chaehy <chlee7670@cau.ac.kr>
2022-07-11 00:09:46 +0900

    style: add mytrashcan page card flex

--- frontend/src/component/Mypage/MyTrashcan.js
+++ frontend/src/component/Mypage/MyTrashcan.js
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { alpha, createTheme, ThemeProvider } from "@mui/material/styles";
 import {
-    Divider,
+    Grid,
     Button,
     CssBaseline,
     TextField,
@@ -10,7 +10,10 @@ import {
     Container,
     Link,
     styled,
+    Switch,
+    Grow
 } from "@mui/material";
+import MultiActionAreaCard from "./MultiActionAreaCard";
 
 const theme = createTheme({
     palette: {
@@ -20,18 +23,87 @@ const theme = createTheme({
     },
 });
 
+const GreenSwitch = styled(Switch)(({ theme }) => ({
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      color: "#76F2BE",
+      '&:hover': {
+        backgroundColor: alpha("#76F2BE", theme.palette.action.hoverOpacity),
+      },
+    },
+    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+      backgroundColor: "white",
+    },
+  }));
+
 function MyTrashcan() {
     return(
         <Container
             style={{
-                backgroundColor: "white",
                 border: "solid",
-                borderColor: "white",
                 borderRadius: 5,
+                borderColor:"transparent",
                 minWidth: "100%",
                 height: "100vh",
             }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between"
+                    }}>
+                    <Typography
+                        color="black"
+                        fontWeight="bold"
+                        sx={{mt: 1.2,
+                            mb: 1,
+                            fontSize: "medium"}}>
+                        내 분리수거함
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                        }}>
+                        <Typography
+                        color="black"
+                        sx={{mt: 2,
+                            mb: 1,
+                            fontSize: 2}}>
+                        사진 자동으로 추가
+                    </Typography>
+                    <GreenSwitch
+                        defaultChecked size="small"
+                        style={{ color: "primary", backgoundColor: "#E7F5EF"}}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                        sx={{mt: 1.5,}}
+                        />
+                    </Box>
+                </Box>               
+                <Container
+                    style={{
+                        backgroundColor: "white",
+                        border: "solid",
+                        borderRadius: 5,
+                        borderColor: "black",
+                        height: "100vh",
+                        pt:2, pb:2}}>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                                alignItems: "center",
+                                justifyContent: "space-evenly"
+                            }}>
+                            <MultiActionAreaCard />
+                            <MultiActionAreaCard />
+                            <MultiActionAreaCard />
+                            <MultiActionAreaCard />
+                            <MultiActionAreaCard />
+                            <MultiActionAreaCard />
 
+                        </Box>                                                
+                </Container>
+            
         </Container>
 
     );
