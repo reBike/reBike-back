86c2d510c00e2eeee049627e6c9bcac15181498a
KyoRuMan <ryokuman@naver.com>
2022-07-13 22:41:19 +0900

    set_up

--- backend/rebikeuser/models.py
+++ backend/rebikeuser/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     user_pw = models.CharField(max_length=64)
     user_email = models.CharField(unique=True, max_length=50)
 
+    def __str__(self):
+        return self.user_name
+
     class Meta:
         managed = False
         db_table = 'User'
