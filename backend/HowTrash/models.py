051f4572713ef2337bface7e859c6174776c4a98
KyoRuMan <ryokuman@naver.com>
2022-07-13 15:19:31 +0900

    find err

--- backend/HowTrash/models.py
+++ backend/HowTrash/models.py
@@ -1,12 +1,6 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from distutils.command.upload import upload
 from django.db import models
+
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,6 +12,7 @@ class User(models.Model):
     class Meta:
         managed = False
         db_table = 'User'
+
 
 class Trashkinds(models.Model):
     trash_kind = models.CharField(primary_key=True, max_length=30)
@@ -38,5 +33,3 @@ class Uploadedtrash(models.Model):
     class Meta:
         managed = False
         db_table = 'UploadedTrash'
-
-
