e7a3981e5c082aed12bfc1be7df8e59fc6804d33
KyoRuMan <ryokuman@naver.com>
2022-07-14 22:39:10 +0900

    end

--- backend/rebikeuser/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'user_alias', 'user_email']
-    search_fields = ['id']
--- backend/rebikeuser/serializers.py
+++ backend/rebikeuser/serializers.py
@@ -7,15 +7,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['user_name', 'user_alias', 'user_email']
-
-
-def user_to_dict(user):
-    result = {
-        'id': user.id,
-        'user_name': user.user_name,
-        'user_alias': user.user_alias,
-        'user_pw': user.user_pw,
-        'user_salt': user.user_salt,
-        'user_email': user.user_email,
-    }
-    return result
