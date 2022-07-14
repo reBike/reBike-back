17e31bea1716178ba83992e554bb8b85d586e334
KyoRuMan <ryokuman@naver.com>
2022-07-14 23:32:26 +0900

    end

--- backend/rebikeuser/views.py
+++ backend/rebikeuser/views.py
@@ -9,17 +9,17 @@ def user_login(request):
     input_id = request.GET.get('id', '')
     input_pw = request.GET.get('pw', '')
     is_login = False
-    a = None
+    user_data = None
 
     if input_pw != '' and input_id != '':
         user = user_findByName(input_id).first()
         if user:
             is_login = user_compPW(input_pw, user)
             if is_login:
-                a = UserSerializer(user)
+                user_data = UserSerializer(user)
 
     data = {
-        'user': a,
+        'user': user_data,
         'is_login': is_login,
     }
 
