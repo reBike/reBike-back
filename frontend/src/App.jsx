ecfe23757c9685c83f74a4deb113ae67d8680d02
jinhobyeong <jhb_0921@naver.com>
2022-07-14 17:03:07 +0900

    style : update uploadpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,38 +6,42 @@ import MyPage from "./Page/MyPage";
 import MainPage from "./Page/MainPage";
 import Header from "./component/Header";
 
-import {ReactComponent as GreenBack } from './images/greenBack.svg'
+import { ReactComponent as GreenBack } from "./images/greenBack.svg";
 import MyTrashcan from "./component/Mypage/MyTrashcan";
 import MyTrashChart from "./component/Mypage/MyTrashChart";
 import ChangeInfo from "./component/Mypage/ChangeInfo";
 import SearchResult from "./component/mainpage/SearchResult";
 
-
 function App() {
-    return (
-        <div>
-            <div style={{position: 'absolute',float: "right", zIndex: -100, top: 0, right: 0}}>
-                <GreenBack />
-            </div>
-            <Router>
-            <Header />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/mainpage" element={<MainPage />} />
-                    <Route path="/mainpage/resultpage" element={<SearchResult/>}/>
-                    <Route path="/mypage" element={<MyPage />}>
-                        <Route index element={<MyTrashcan />} />
-                        <Route path='/mypage/myTrashChart' element={<MyTrashChart />} />
-                        <Route path='/mypage/userInfo' element={<ChangeInfo />} />
-                        <Route path='/mypage/logout' element={<MyTrashcan />} />
-                    </Route>
-                    
-
-                </Routes>
-            </Router>
-        </div>
-        
-    );
+  return (
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          float: "right",
+          zIndex: -100,
+          top: 0,
+          right: 0,
+        }}
+      >
+        <GreenBack />
+      </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/mainpage" element={<MainPage />} />
+          <Route path="/mainpage/resultpage" element={<SearchResult />} />
+          <Route path="/mypage" element={<MyPage />}>
+            <Route index element={<MyTrashcan />} />
+            <Route path="/mypage/myTrashChart" element={<MyTrashChart />} />
+            <Route path="/mypage/userInfo" element={<ChangeInfo />} />
+            <Route path="/mypage/logout" element={<MyTrashcan />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/component/mainpage/SearchResult.jsx
+++ frontend/src/component/mainpage/SearchResult.jsx
@@ -1,53 +1,64 @@
 import * as React from "react";
-import {Box,Button, CardMedia, Typography} from "@mui/material";
+import { Box, Button, CardMedia, Typography } from "@mui/material";
 
-import InputIcon from '@mui/icons-material/Input';
-
-
-
-
-
+import InputIcon from "@mui/icons-material/Input";
+import { useLocation } from "react-router";
 
 const SearchResult = () => {
+  const { state } = useLocation(); //이미지 주소
   return (
- 
-    <Box textAlign={"center"} >
+    <Box textAlign={"center"}>
       <div>
-        <Box sx={{ borderRadius : 3, border : 1 ,borderColor :"black" ,backgroundColor: "white",width: 600,height: 300, margin:'auto',mt:23,}} >
-            <CardMedia
-                component="img"
-                width ="600"
-                height="300"
-                image='https://picsum.photos/400/300'
-                sx={{borderRadius : 3}}
-            />
+        <Box
+          sx={{
+            borderRadius: 3,
+            border: 1,
+            borderColor: "black",
+            backgroundColor: "white",
+            width: 600,
+            height: 300,
+            margin: "auto",
+            mt: 23,
+          }}
+        >
+          <CardMedia
+            component="img"
+            width="600"
+            height="300"
+            image={state}
+            sx={{ borderRadius: 3 }}
+          />
         </Box>
 
-        <Typography  
-            marginTop={5}
-            fontWeight="bold"
-            variant="h5" >
-            결과 : 물병 
-            {/* 나중에 값 넘겨 받아서 하면됨 아니면 결과값 따로 받아서 하면 될 듯 */}
+        <Typography marginTop={5} fontWeight="bold" variant="h5">
+          결과 : 물병
+          {/* 나중에 값 넘겨 받아서 하면됨 아니면 결과값 따로 받아서 하면 될 듯 */}
         </Typography>
 
-        <Button  sx={{
-            backgroundColor : "white",
-            color : "#759F98",
-            border : 2,
-            borderRadius : 2, margin:"auto", mt : 6,width: "50ch" }}>
-       
-            <Typography sx = {{ color : "black"}} fontSize={17} marginRight={2} fontWeight = "bold">
-                쓰레기 올바르게 버리는 방법
-            </Typography>
-            <InputIcon fontSize="medium" sx = {{ color : "black"}} />
+        <Button
+          sx={{
+            backgroundColor: "white",
+            color: "#759F98",
+            border: 2,
+            borderRadius: 2,
+            margin: "auto",
+            mt: 6,
+            width: "50ch",
+          }}
+        >
+          <Typography
+            sx={{ color: "black" }}
+            fontSize={17}
+            marginRight={2}
+            fontWeight="bold"
+          >
+            쓰레기 올바르게 버리는 방법
+          </Typography>
+          <InputIcon fontSize="medium" sx={{ color: "black" }} />
         </Button>
-      
       </div>
     </Box>
   );
 };
 
 export default SearchResult;
-
-
