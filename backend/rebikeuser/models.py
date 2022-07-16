2a9b40ef2d24f03023a60b9302d7b386bf6a2ec8
SeongbinPark <103374034+SeongbinPark@users.noreply.github.com>
2022-07-17 01:59:25 +0900

    회원가입 DRF

--- backend/rebikeuser/models.py
+++ backend/rebikeuser/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 import uuid
 
-
 class user(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
-    name = models.CharField(unique=True, max_length=20)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(unique=True, max_length=20, null=True, blank=True)
     alias = models.CharField(unique=True, max_length=20)
     pw = models.BinaryField(max_length=60)
     salt = models.BinaryField(max_length=29)
     email = models.CharField(unique=True, max_length=50)
+    active = models.IntegerField(default=1)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'user'
+#, default=str(uuid.uuid4())
+
+
