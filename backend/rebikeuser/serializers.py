714e21d7cdea956ebe6f8198de4109bd2457b74a
SeongbinPark <103374034+SeongbinPark@users.noreply.github.com>
2022-07-17 01:58:44 +0900

    회원가입 DRF

--- backend/rebikeuser/serializers.py
+++ backend/rebikeuser/serializers.py
@@ -7,3 +7,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
         fields = ['name', 'alias', 'email'] #Front에 3필드만
+
+        
+class SignupInput(serializers.ModelSerializer):
+    # 검증부
+    email=serializers.EmailField()
+    pw=serializers.CharField(max_length=60)
+    alias=serializers.CharField(max_length=20)
+    name=serializers.CharField(max_length=20)
+    class Meta:
+        model=user
+        fields=['name', 'pw', 'alias', 'email']#실제 response할 필드
+        
+#rebikeuser/serializers.py
+class UserSignupResponse(serializers.ModelSerializer):
+    class Meta:
+        model = user
+        fields = ['id']#프론트에주는 값
+        
+        
+        
+        
+        
