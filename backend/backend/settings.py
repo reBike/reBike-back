ddc8c5da96ee49a931973c180e3aecb91a36b5a7
Ellie <doris0707@naver.com>
2022-07-18 13:45:43 +0900

    mypage-final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,11 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- backend/rebiketrash/urls.py
+++ backend/rebiketrash/urls.py
@@ -2,16 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('mainpage/trashkind/',views.TrashkindListAPI.as_view()),
+    #path('mainpage/users/<user_id>/<input_image>',views.CreateImage.as_view()),
+    path('mainpage',views.CreateImage.as_view()),
 
-    path('mypage/<user_id>/image/',views.histories, name='histories'),
-    path('mypage/<user_id>/image/<uploaded_trash_image_id>/',views.UploadedtrashimageListAPI.as_view()),
-    #path('mypage/<user_id>/statistics/',views.statistics, name='statistics'),
+    path('mypage/users/<user_id>/images',views.histories, name='histories'),
+    path('mypage/users/<user_id>/images/<uploaded_trash_image_id>',views.UploadedtrashimageListAPI.as_view()),
+    path('mypage/users/<user_id>/statistics',views.statistics, name='statistics'),
 ]
 
-
-### 유저 페이지 진입 시 액티브된 계정인지(있는지 없는지 확인)
-### 통계페이지 (액티브 상관 없이 받아오기!)
 ### 자주 쓰이는 코드 유틸화 trashUtils.py
-
-### swagger, postman 확인 -> 성빈님
