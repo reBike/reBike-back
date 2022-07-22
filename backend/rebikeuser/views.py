8bf1fbb184ae15006eac6af9ceb922cb8085a8f9
KyoRuMan <ryokuman@naver.com>
2022-07-22 17:23:12 +0900

    ADD decode_token

--- backend/rebikeuser/views.py
+++ backend/rebikeuser/views.py
@@ -2,10 +2,20 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import UserSignupResponse, AutoUpload
+from .serializers import UserSignupResponse
 from .userUtil import user_find_by_name, user_compPW, user_create_client, user_change_pw, user_change_alias, \
-    user_generate_access_token, user_generate_refresh_token, user_token_to_data, user_duplicate_check, user_deactivate,\
+    user_generate_access_token, user_generate_refresh_token, user_token_to_data, user_duplicate_check, user_deactivate, \
     user_refresh_to_access
+
+
+@api_view(['POST'])
+def user_decode_token(request):
+    access_token = request.headers.get('Authorization')
+    payload = user_token_to_data(access_token)
+    if payload:
+        return JsonResponse({'name': payload.name, 'alias': payload.alias, 'email': payload.email}, status=200)
+    else:
+        return JsonResponse({"message": payload}, status=403)
 
 
 @api_view(['POST'])
@@ -31,13 +41,13 @@ def user_login(request):
     refresh_token = None
 
     if input_pw and input_name:
-        user = user_find_by_name(input_name).first()
-        if user:
-            if user_compPW(input_pw, user):
-                access_token = user_generate_access_token(user)
-                refresh_token = user_generate_refresh_token(user)
+        user_data = user_find_by_name(input_name).first()
+        if user_data:
+            if user_compPW(input_pw, user_data):
+                access_token = user_generate_access_token(user_data)
+                refresh_token = user_generate_refresh_token(user_data)
 
-    data = {"access_token": access_token, "refresh_token": refresh_token, "save_img": user.save_img}
+    data = {"access_token": access_token, "refresh_token": refresh_token, "save_img": user_data.save_img}
     return Response(data)
 
 
@@ -115,15 +125,8 @@ def user_set_autosave(request):
     payload = user_token_to_data(access_token)
     if payload:
         user = user_find_by_name(payload.name).first()
-        user.save_img=save_img
+        user.save_img = save_img
         user.save()
-        # serializer = AutoUpload(data={"save_img": user.save_img})
-        # if serializer.is_valid():
-        #     data = {
-        #         "save_img": serializer.data
-        #     }
-        #     return JsonResponse(data)
-
-# 로그인 => 엑세스토큰 , 리프레시 토큰, 세이브이미지
-# 토글 다다다다다 => 로컬스토리지
-# 엑세스토큰 재발급 => db 접근, 세이브이미지 값 변경해주기
+        return JsonResponse({"message": "success"}, status=200)
+    else:
+        return JsonResponse({"message": payload}, status=403)
