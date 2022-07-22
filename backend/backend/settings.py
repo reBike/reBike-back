caab95cc6b71134730ae41e8251b89067f4781db
KyoRuMan <ryokuman@naver.com>
2022-07-22 10:19:00 +0900

    temp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import os
 import environ
 
-is_dev = False
+is_dev = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
