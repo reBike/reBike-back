c61fece744d7295b46a0b4ff3de6ea0f9e20ba6f
SeongbinPark <103374034+SeongbinPark@users.noreply.github.com>
2022-07-28 17:29:10 +0900

    Update serializers.py

--- backend/rebikeuser/serializers.py
+++ backend/rebikeuser/serializers.py
@@ -1,42 +1,14 @@
-import uuid
 from rest_framework import serializers
 from .models import user
-import uuid
 
 
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=20)
-    alias = serializers.CharField(max_length=20)
-    email = serializers.CharField(max_length=50)
-    id = serializers.UUIDField(default=uuid.uuid4)
-
-    class Meta:
-        model = user
-        fields = ['name', 'alias', 'email', 'id']  # Front에 3필드만
-
-
-# rebikeuser/serializers.py
 class UserSignupResponse(serializers.ModelSerializer):
     class Meta:
         model = user
         fields = ['name']  # 프론트에주는 값
 
-
-class SignupInput(serializers.ModelSerializer):
-    # 검증부
-    email = serializers.CharField(max_length=50)
-    pw = serializers.CharField(max_length=60)
-    alias = serializers.CharField(max_length=20)
-    name = serializers.CharField(max_length=20)
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = user
-        fields = ('email', 'pw', 'alias', 'name')  # 프론트에주는 값
-
-
-class AutoUpload(serializers.ModelSerializer):
-    save_img = serializers.IntegerField(default=1)
-
-    class Meta:
-        model = user
-        fields = ['save_img']
+        fields = ['id', 'name', 'alias', 'pw', 'email']
