e7de3afc654501955f0fd5f6960b8d159f0d8e16
박성빈 <103374034+SeongbinPark@users.noreply.github.com>
2022-08-01 01:37:05 +0900

    Update urls.py
    Modified config.url.py ( Add /api/ )

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,9 +7,9 @@ from drf_yasg import openapi
 
 schema_view_v1 = get_schema_view(
     openapi.Info(
-        title="Open API",
+        title="reBike APIs",
         default_version='v1',
-        description="간단한 설명",
+        description="reBike 프로젝트 API 목록입니다.",
         terms_of_service="https://www.google.com/policies/terms/",
     ),
     public=True,
@@ -18,8 +18,8 @@ schema_view_v1 = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('rebikeuser.urls')),
-    path('trash/', include('rebiketrash.urls')),
+    path('/api/users/', include('rebikeuser.urls')),
+    path('/api/trash/', include('rebiketrash.urls')),
     path('', include('django_prometheus.urls')),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
