d660f79b5e82ed6bf84a334d2731055b3b213806
KyoRuMan <ryokuman@naver.com>
2022-08-01 11:02:52 +0900

    temp

--- backend/elastic_search/urls.py
+++ backend/elastic_search/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.elastic_search),
+    path('name/', views.elastic_search_get_by_name),
+    path('desc/', views.elastic_search_get_by_desc),
+    path('', views.elastic_search_post),
 ]
--- backend/elastic_search/views.py
+++ backend/elastic_search/views.py
@@ -1,18 +1,16 @@
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
+from elasticsearch_dsl import Search
+from elasticsearch import Elasticsearch
+
 from .models import Category
 from .documents import CategoryDocument
 
-
-@api_view(['GET', 'POST'])
-def elastic_search(request):
-    if request.method == 'GET':
-        return elastic_search_get(request)
-    if request.method == 'POST':
-        return elastic_search_post(request)
+client = Elasticsearch()
 
 
-def elastic_search_get(request):
+@api_view(['GET'])
+def elastic_search_get_by_name(request):
     search_value = request.GET.get('value')
     search_result = CategoryDocument.search().filter("term", name=search_value)
     return_result = []
@@ -22,6 +20,7 @@ def elastic_search_get(request):
     return JsonResponse({'result': return_result}, status=200)
 
 
+@api_view(['POST'])
 def elastic_search_post(request):
     name = request.data['name']
     desc = request.data['desc']
@@ -34,3 +33,14 @@ def elastic_search_post(request):
     category.save()
     return JsonResponse({'check_name': check_name, 'check_desc': check_desc, 'name': name, 'desc': "desc"},
                         status=201)
+
+
+@api_view(['GET'])
+def elastic_search_get_by_desc(request):
+    search_value = request.GET.get('value')
+    search_result = Search().using(client).query("match", desc=search_value)
+    return_result = []
+    num = 1
+    for hit in search_result:
+        return_result.append(dict({'number': num, 'name': hit.name, 'desc': hit.desc}))
+    return JsonResponse({'result': return_result}, status=200)
