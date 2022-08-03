7670463135672e05e474eb71ed7badb90bf1a646
Ellie <doris0707@naver.com>
2022-08-03 16:46:19 +0900

    reCelery

--- backend/rebiketrash/views.py
+++ backend/rebiketrash/views.py
@@ -1,3 +1,4 @@
+from unittest import result
 from django.db.models import Count
 from django.http import JsonResponse
 
@@ -175,27 +176,32 @@ def get_task_id(request, user_id):
 def get_task_result(request, user_id, task_id):
     task = AsyncResult(task_id)
     if not task.ready():  # 작업이 완료되지 않았을 경우
-        return Response(status=status.HTTP_100_CONTINUE)
+        return JsonResponse({"ai_result":"notyet"})
 
     ai_results = task.get("ai_results")
     image_url = task.get("image_url")
 
-    if ai_results['ai_results'] == 0:  # 사진이 분류되지 않을 경우
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    if ai_results['ai_results'] == 0:  # 재활용할 쓰레기가 없는 경우
+        return JsonResponse({"ai_result":"false"})
+    
+    try:
+        trash_image.objects.get(image = image_url["image_url"])
+        return JsonResponse({"ai_result":"exist"})
+    except trash_image.DoesNotExist:
+        user_info = user.objects.get(id=user_id)
+        trash_image.objects.create(
+            active=user_info.autosave, image=image_url["image_url"], user_id=user_info)
 
-    user_info = user.objects.get(id=user_id)
-    trash_image.objects.create(
-        active=user_info.autosave, image=image_url["image_url"], user_id=user_info)
+        image_info = trash_image.objects.get(
+            image=image_url["image_url"], user_id=user_info)
 
-    image_info = trash_image.objects.get(
-        image=image_url["image_url"], user_id=user_info)
+        for ai_result in ai_results['ai_results'].split(" "):
+            trash_kind.objects.create(
+                trash_image_id=image_info, user_id=user_info, kind=ai_result)
 
-    for ai_result in ai_results['ai_results'].split(" "):
-        trash_kind.objects.create(
-            trash_image_id=image_info, user_id=user_info, kind=ai_result)
-
-    # 챌린지 달성 여부 조사
-    challenge_id, challenge_content = check_challenge(user_id)
-
-    return JsonResponse(
-        {'image_id': image_info.id, 'challenge_id': challenge_id, 'challenge_content': challenge_content})
+        # 챌린지 달성 여부 조사
+        challenge_id, challenge_content = check_challenge(user_id)
+        
+        return JsonResponse(
+            {'image_id': image_info.id, 'challenge_id': challenge_id, 'challenge_content': challenge_content})
+    
