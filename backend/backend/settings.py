353ec7f2dae1597eb2eb8ae34da82502f7ad73c3
Ryokuman <ryokuman21@gmail.com>
2022-08-04 15:16:36 +0900

    mod: docker-compose.yml (delete: mysqldb , frontend //only for dev ) mod: nginx ( for ec2 )

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 import environ
 
-is_dev = True
+is_dev = False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
